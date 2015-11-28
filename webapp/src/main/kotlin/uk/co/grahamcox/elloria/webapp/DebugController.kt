d146f5b969930b208786f9b7408b2e4367e9fe3b
Graham Cox <graham@grahamcox.co.uk>
2015-11-28 22:41:41 +0000

    Debug controller supporting Ping, Now and Status handlers

---
+++ webapp/src/main/kotlin/uk/co/grahamcox/elloria/webapp/DebugController.kt
@@ -0,0 +1,37 @@
+package uk.co.grahamcox.elloria.webapp
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import java.time.Clock
+
+/**
+ * Controller for performing debug operations
+ * @property clock The clock to use
+ */
+@Controller
+@RequestMapping("/api/debug")
+class DebugController(private val clock: Clock) {
+    /**
+     * Trivial handler that simply sends a response back
+     */
+    @ResponseBody
+    @RequestMapping("/ping")
+    fun ping() = "Pong"
+
+    /**
+     * Get the current server time
+     */
+    @ResponseBody
+    @RequestMapping("/now")
+    fun now() = clock.instant()
+
+    /**
+     * Generate a response with the given status code
+     */
+    @RequestMapping("/status/{statusCode:\\d\\d\\d}")
+    fun status(@PathVariable statusCode: Int) = ResponseEntity<Any?>(HttpStatus.valueOf(statusCode))
+}
