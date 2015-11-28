7b7df45d1182e1cbc66a6c957b8b430365f283cb
Graham Cox <graham@grahamcox.co.uk>
2015-11-28 21:30:47 +0000

    Added in the Servlet Context

---
+++ webapp/src/main/kotlin/uk/co/grahamcox/elloria/webapp/spring/WebMvcContext.kt
@@ -0,0 +1,13 @@
+package uk.co.grahamcox.elloria.webapp.spring
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+/**
+ * The configuration to use for the Spring MVC Configuration
+ */
+@Configuration
+@EnableWebMvc
+open class WebMvcContext : WebMvcConfigurerAdapter() {
+}
