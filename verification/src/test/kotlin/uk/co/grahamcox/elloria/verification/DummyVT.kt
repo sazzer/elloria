664a16be840412d9689c99aaa15832346ec80939
Graham Cox <graham@grahamcox.co.uk>
2015-11-28 21:58:56 +0000

    Dummy verification test that always fails, and make sure it only runs with a particular profile

---
+++ verification/src/test/kotlin/uk/co/grahamcox/elloria/verification/DummyVT.kt
@@ -0,0 +1,16 @@
+package uk.co.grahamcox.elloria.verification
+
+import org.junit.Test
+
+/**
+ * Dummy Verification Test
+ */
+class DummyVT {
+    /**
+     * Dummy Test
+     */
+    @Test
+    fun test() {
+        throw UnsupportedOperationException()
+    }
+}
